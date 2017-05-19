416f8a5688e6fb2adc3d8843edb4025f930c461f
alice.edn@gmail.com <alice.edn@gmail.com>
2017-05-19 16:52:40 +0200

    check column

--- takuzu.php
+++ takuzu.php
@@ -1,12 +1,52 @@
 <?php
 
 $nb=0;
-$grille = generate_h($nb);
-$grille = generate_h($nb,$grille);
-
-
-
+$grille = array();
+$grille = generate_h($grille);
 display_grid($grille);
+echo "\n";
+$grille = check_grid($grille);
+display_grid($grille);
+
+function check_grid($grille){
+    $grille = return_grid($grille);
+    $grid_wrong = false;
+    echo "\n";
+    display_grid($grille);
+
+    for ($i = 0; $i < 8; $i++) {
+        while(checkIdenticalLine($i, $grille, $grille[$i])) {
+            $grille[$i] = generate_line ();
+            $grid_wrong = true;
+        }
+
+        $grille[$i] = check_triplon($grille[$i]);
+
+    }
+
+    /*sleep(5);*/
+    echo "\n";
+    display_grid($grille);
+
+
+    while($grid_wrong = true) {
+        check_grid($grille);
+    }
+    return $grille;
+}
+
+
+function check_triplon($line) {
+    for ($j = 0; $j < 8; $j++) {
+        if ($j > 1 && $line[$j - 1] == $line[$j - 2]) {
+            if ($line[$j - 1] == 0) $line[$j] = 1;
+            else $line[$j] = 0;
+        }else  $line[$j] = rand(0, 1);
+    }
+
+    while (array_sum($line) != 4) { $line = generate_line (); check_triplon($line); }
+    return $line;
+}
 
 function return_grid($grille){
     $grille2 = array();
@@ -28,33 +68,21 @@ function display_grid($grille) {
     }
 }
 
-function generate_h($nb,$grille=null) {
+function generate_h() {
 
     for ($i = 0; $i < 8; $i++) {
-        $good_grid = true;
-
-        if(is_null($grille)) $grille[$i] = generate_line ();
+        $grille[$i] = generate_line ();
 
         while(checkIdenticalLine($i, $grille, $grille[$i])) {
             $grille[$i] = generate_line ();
-            $nb=0;
-            $good_grid = false;
         }
-    }
-
-    if(!is_null($grille)) while( $nb < 2) {
-        echo "check grille again\n";
-        echo $nb + "\n";
-        if($good_grid = true) $nb ++;
-        $grille = return_grid($grille);
-        $grille = generate_h($nb,$grille);
     }
 
     return $grille;
 }
 
 function checkIdenticalLine($nb, $grille, $current_line){
-    for ($i = 0; $i <= $nb-1; $i++) {
+    for ($i = 0; $i < $nb; $i++) {
         if ($grille[$i] === $current_line) {
             return true;
         }
@@ -66,10 +94,10 @@ function checkIdenticalLine($nb, $grille, $current_line){
 function generate_line () {
     $line = array();
     while(array_sum($line) != 4 ) {
-        for ($i = 0; $i < 8; $i++) {         
+        for ($i = 0; $i < 8; $i++) {
             if ($i > 1 && $line[$i - 1] == $line[$i - 2]) {
                 if ($line[$i - 1] == 0) $line[$i] = 1;
-                elseif ($line[$i - 1] == 1) $line[$i] = 0;
+                else $line[$i] = 0;
             }else  $line[$i] = rand(0, 1);
         }
     }
