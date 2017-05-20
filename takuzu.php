5abf6551037d2e46b7b3f8ab2a0ea3942745be80
alice.edn@gmail.com <alice.edn@gmail.com>
2017-05-20 15:38:48 +0200

    Html in progress

--- takuzu.php
+++ takuzu.php
@@ -1,29 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Tableau html simple</title>
+    <style>
+        td {border: 1px black solid; text-align: center; width: 25px; height: 25px;}
+    </style>
+</head>
+<body>
 
-//variables
-$grille = array ();
-$compteur = 0;
-$bad = 0;
-$newGrid = 0;
-$timestamp_debut = microtime(true);
-//on remplit la grille
-$grille = generateGrid ();
-echo "\n";
-echo "Calcul en cours ... on attend ...\n\n";
-//tant que le compteur de tableau non modifié est inférieur à 5 on tourne la grille et on check
-while ( $compteur < 5 ) {
-    $grille = modifyGrid($grille);
-    $grille = returnGrid($grille);
-    if (checkGridCorrect($grille)) {
-        $compteur++;
-    } else {
-        $compteur = 0;
-        $bad++;
+<table>
+    <tbody>
+    <?php
+
+    //variables
+    $grille = array ();
+    $compteur = 0;
+    $bad = 0;
+    $newGrid = 0;
+    $timestamp_debut = microtime(true);
+    //on remplit la grille
+    $grille = generateGrid ();
+    echo "\n";
+    echo "Calcul en cours ... on attend ...\n\n";
+    //tant que le compteur de tableau non modifié est inférieur à 5 on tourne la grille et on check
+    while ( $compteur < 5 ) {
+        $grille = modifyGrid($grille);
+        $grille = returnGrid($grille);
+        if (checkGridCorrect($grille)) {
+            $compteur++;
+        } else {
+            $compteur = 0;
+            $bad++;
+        }
+        if ($bad>1000) { $newGrid++; $grille = generateGrid (); $bad = 0; }
     }
-    if ($bad>1000) { $newGrid++; $grille = generateGrid (); $bad = 0; }
-}
-//on affiche la grille correcte
-displayGrid($grille);
+    // pour chaque ligne
+    for ($i=1; $i<8; $i++)
+    {
+        ?>
+        <tr>
+            <?php		// pour chaque colonne (de la ligne)
+            for ($j=1; $j<8; $j++)
+            {
+                ?>		<td>
+                <?php
+                echo $grille[$i][$j];
+                ?>		</td>
+            <?php	} // end for
+            ?>
+        </tr>
+        <?php
+    } // end for
+    ?>
+    </tbody>
+</table>
+
+</body>
+</html>
+
+<?php
 echo "On a modifié ".$bad." fois la grille\n";
 echo "On a abandonné et regénéré ".$newGrid." fois une nouvelle grille\n";
 $timestamp_fin = microtime(true);
